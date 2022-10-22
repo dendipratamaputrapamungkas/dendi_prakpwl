8288846dbedc20f0d02f2794b3fb040e42c82522
dendipratamaputrapamungkas <dendipasper33@gmail.com>
2022-10-22 09:44:01 +0700

    pertemuan 3 web lanjut praktikum laravel dasar

---
+++ resources/views/test.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>test laravel</title>
+</head>
+<body>
+    <h1>Selamat datang Bapak Doesn't</h1>
+    <ul>
+        @foreach ($nama as $n)
+        <li>{{$n}}</li>
+        @endforeach
+    </ul>
+    <a href="#">WOke</a>
+</body>
+</html>
